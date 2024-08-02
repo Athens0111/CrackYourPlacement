fdca1eb00be73f425cf818aabe7ab6230eacd374
ATHARV BARDAPURKAR <119130012+Athens0111@users.noreply.github.com>
2024-08-02 16:11:12 +0530

    Time: 8 ms (54.16%), Space: 21.6 MB (12.28%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,22 +1,31 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
+        // Create a temporary vector to store unique elements
         vector<int> temp;
-        int k=1,t=nums[0];
+        // Initialize a counter for unique elements and set t to the first element
+        int k = 1, t = nums[0];
 
-        for(int i=0;i<nums.size();i++){
-            if(i==0){
+        // Iterate through the input vector
+        for (int i = 0; i < nums.size(); i++) {
+            // For the first element, add it to the temporary vector
+            if (i == 0) {
                 temp.push_back(nums[i]);
-            }
-            else{
-                if(nums[i]!=t){
-                    t=nums[i];
+            } else {
+                // If the current element is different from the last unique element
+                if (nums[i] != t) {
+                    // Update t to the current element and add it to the temporary vector
+                    t = nums[i];
                     temp.push_back(t);
+                    // Increment the counter for unique elements
                     k++;
                 }
             }
         }
+        // Replace the original vector with the temporary vector containing unique elements
         nums = temp;
+        // Return the count of unique elements
         return k;
     }
+
 };
